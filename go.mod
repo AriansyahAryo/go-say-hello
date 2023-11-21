531b27494be5f62b224db58aa24acd695bef85b4
ariansyaharyo <ariansyaharyo@gmail.com>
2023-11-21 08:52:29 +0700

    update module say hello dengan parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/AriansyahAryo/go-say-hello
+//yang berubah nama modulenya aja ditambah v2 diakhirnya
+//module github.com/AriansyahAryo/go-say-hello
+module github.com/AriansyahAryo/go-say-hello/v2
 
 go 1.21.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,10 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World"
+//func SayHello() string {
+//	return "Hello World"
+//}
+
+// anggap aja major changes
+func SayHello(nama string) string {
+	return "Hello " + nama
 }
